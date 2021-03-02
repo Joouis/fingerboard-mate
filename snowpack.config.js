9f2715cebd4072b2db34cf83764e0c3aaf0f7d66
Joou <luckyjoou@gmail.com>
2021-03-02 15:20:20 +0800

    Init

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,12 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    /* ... */
+    public: { url: '/', static: true },
+    src: { url: '/dist' }
   },
   plugins: [
-    /* ... */
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv'
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
