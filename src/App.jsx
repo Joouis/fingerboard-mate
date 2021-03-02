3ea89d6e8f8bf7b09ff048317d5cc29a45059c3e
Joou <luckyjoou@gmail.com>
2021-03-02 16:28:49 +0800

    Add refresh interval controll

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { IntervalTester } from "./IntervalTester";
+import 'antd/dist/antd.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  // TODO: allow to set refreshTime
-  const [refreshTime] = useState(30);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), refreshTime * 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount, refreshTime]);
-  // Return the App component.
   return (
     <div className="App">
       <IntervalTester />
--- src/IntervalTester.jsx
+++ src/IntervalTester.jsx
@@ -1,17 +1,35 @@
+import { InputNumber } from "antd";
 import { sample } from "lodash";
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 const Strings = ['1st', '2nd', '3rd', '4th', '5th', '6th'];
 const Roots = ['C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'F', 'F#', 'Gb', 'G', 'G#', 'Ab', 'A', 'A#', 'Bb', 'B'];
 const Intervals = ['m2', 'M2', 'm3', 'M3', 'P4', 'd5', 'A4', 'P5', 'm6', 'M6', 'm7', 'M7', 'P8'];
 
 export const IntervalTester = () => {
+  const [count, setCount] = useState(0);
+  const [refreshTime, setRefreshTime] = useState(30);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setCount(count + 1), refreshTime * 1000);
+    return () => clearTimeout(timer);
+  }, [count, setCount, refreshTime]);
+
+  const _onChange = useCallback((newVal) => {
+    if (newVal && !isNaN(newVal)) {
+      setRefreshTime(newVal);
+    }
+  }, [setRefreshTime]);
+
   return (
     <div>
       <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "40vh" }}>
         <div>Please perform:<br /><br /></div>
         <div>
-          <b style={{ fontSize: "1.5rem" }}>{sample(Roots)}</b> on <b style={{ fontSize: "1.5rem" }}>{sample(Strings)}</b> string and all <b style={{ fontSize: "1.5rem" }}>{sample(Intervals)}</b> notes
+          <b style={{ fontSize: "1.5rem" }}>{sample(Roots)}</b> on <b style={{ fontSize: "1.5rem" }}>{sample(Strings)}</b> string and its all <b style={{ fontSize: "1.5rem" }}>{sample(Intervals)}</b> notes
+        </div>
+        <div style={{ marginTop: "3vh" }}>
+          Refresh seconds: <InputNumber min={1} max={10000} defaultValue={refreshTime} onChange={_onChange} />
         </div>
       </div>
       <div>
