cfda6c8f4bafb954d6c3379ced2cb7b6523bc9dd
Joou <luckyjoou@gmail.com>
2021-03-02 18:00:12 +0800

    Update

--- scripts/publish.js
+++ scripts/publish.js
@@ -2,7 +2,9 @@ const ghpages = require('gh-pages');
 
 console.log('Start to publish...');
 
-ghpages.publish('build', err => {
+ghpages.publish('build', {
+  repo: "git@github.com:Joouis/fingerboard-mate.git"
+}, err => {
   if (err) {
     console.log(err);
   }
